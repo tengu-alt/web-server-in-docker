4deb1249f12c57890d03a6b7ae2880bd9492aa3e
tengu-alt <luzniyhome1@gmail.com>
2022-03-30 19:40:57 +0300

    newTests

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 build:
 	docker-compose up --build
-mig:
+migup:
 	migrate -database postgres://postgres:12345@localhost:6080/models?sslmode=disable -path ./migrations up
+migdown:
+	migrate -database postgres://postgres:12345@localhost:6080/models?sslmode=disable -path ./migrations down
 down:
 	docker-compose down
--- pkg/store/store_test.go
+++ pkg/store/store_test.go
@@ -12,56 +12,115 @@ func getConnStruct(db *sqlx.DB) *DataBase {
 	}
 }
 
-func TestDBPositive(t *testing.T) {
+type testCase struct {
+	User  models.User
+	salt  string
+	hash  string
+	token string
+}
+
+//func truncateCustomTable(name string) {
+//	connString := "postgres://postgres:12345@localhost:6080/models?sslmode=disable"
+//	database, _ := NewConnect(connString)
+//	conn := getConnStruct(database)
+//	db := conn.DBmodel
+//	_, err := db.Queryx("TRUNCATE TABLE $1", name)
+//	if err != nil {
+//		panic(err)
+//	}
+//}
+
+func Test_InsertUserToPositive(t *testing.T) {
 	u := models.User{
 		FirstName: "123",
 		LastName:  "123",
 		Email:     "123@123.1231",
 	}
-	salt := "QFVgDYtUIzM="
-	hash := "JDJhJDEwJDBvdy9rNk1RdGkwemFaZHYwbWhCRXVKU2x5NEd6ZnBlVGRBYUdsLjFXbWFIcVJ6d2EzNHQu"
+	testcaseDBPos := testCase{
+		u,
+		"QFVgDYtUIzM=",
+		"JDJhJDEwJDBvdy9rNk1RdGkwemFaZHYwbWhCRXVKU2x5NEd6ZnBlVGRBYUdsLjFXbWFIcVJ6d2EzNHQu",
+		"asdasadasd",
+	}
 	connString := "postgres://postgres:12345@localhost:6080/models?sslmode=disable"
-	token := "asdasadasd"
 	database, err := NewConnect(connString)
 	if err != nil {
 		t.Error(err)
 	}
 	conn := getConnStruct(database)
-	t.Run("insert", func(t *testing.T) {
-		err := conn.InsertToDB(u, salt, hash)
-		if err != nil {
-			t.Fatal("cannot insert")
-		}
-	})
-	t.Run("insertToken", func(t *testing.T) {
-		err := conn.InsertToken(u.Email, token)
-		if err != nil {
-			t.Fatal("cannot insert")
-		}
-	})
-	t.Run("dropToken", func(t *testing.T) {
-		err := conn.DropToken(token)
-		if err != nil {
-			t.Fatal("cannot drop")
-		}
-	})
-	t.Run("getNames", func(t *testing.T) {
-		_, _, err := conn.GetNames(u.Email)
-		if err != nil {
-			t.Fatal("cannot get names")
-		}
-	})
-	t.Run("dropToken", func(t *testing.T) {
-		err := conn.DropToken(token)
-		if err != nil {
-			t.Fatal("cannot drop")
-		}
-	})
-	t.Run("checkEmail", func(t *testing.T) {
-		err := conn.CheckMail(u.Email)
-		if err {
-			t.Fatal("cannot check")
-		}
-	})
+	err = conn.InsertToDB(testcaseDBPos.User, testcaseDBPos.salt, testcaseDBPos.hash)
+	if err != nil {
+		t.Fatal("cannot insert")
+	}
+}
 
+func TestDataBase_InsertToken(t *testing.T) {
+	u := models.User{
+		Email: "123@123.1231",
+	}
+	testestcaseDBPos := testCase{
+		User:  u,
+		token: "asdasadasd",
+	}
+	connString := "postgres://postgres:12345@localhost:6080/models?sslmode=disable"
+	database, err := NewConnect(connString)
+	if err != nil {
+		t.Error(err)
+	}
+	conn := getConnStruct(database)
+	err = conn.InsertToken(testestcaseDBPos.User.Email, testestcaseDBPos.token)
+	if err != nil {
+		t.Fatal("cannot insert")
+	}
+}
+func TestDataBase_DropToken(t *testing.T) {
+	testestcaseDBPos := testCase{
+		token: "asdasadasd",
+	}
+	connString := "postgres://postgres:12345@localhost:6080/models?sslmode=disable"
+	database, err := NewConnect(connString)
+	if err != nil {
+		t.Error(err)
+	}
+	conn := getConnStruct(database)
+	err = conn.DropToken(testestcaseDBPos.token)
+	if err != nil {
+		t.Fatal("cannot insert")
+	}
+}
+func TestDataBase_CheckMail(t *testing.T) {
+	u := models.User{
+		Email: "123@123.1231",
+	}
+	testestcaseDBPos := testCase{
+		User: u,
+	}
+	connString := "postgres://postgres:12345@localhost:6080/models?sslmode=disable"
+	database, err := NewConnect(connString)
+	if err != nil {
+		t.Error(err)
+	}
+	conn := getConnStruct(database)
+	err2 := conn.CheckMail(testestcaseDBPos.User.Email)
+	if err2 {
+		t.Fatal("cannot check")
+	}
+}
+func TestDataBase_GetNames(t *testing.T) {
+	u := models.User{
+		Email: "123@123.1231",
+	}
+	testestcaseDBPos := testCase{
+		User: u,
+	}
+	connString := "postgres://postgres:12345@localhost:6080/models?sslmode=disable"
+	database, err := NewConnect(connString)
+	if err != nil {
+		t.Error(err)
+	}
+	conn := getConnStruct(database)
+	_, _, err = conn.GetNames(testestcaseDBPos.User.Email)
+	if err != nil {
+		t.Fatal("cannot get names")
+	}
 }
